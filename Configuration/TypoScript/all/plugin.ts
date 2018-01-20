2d5a4555e329322db0a0cb712681b3792e6270d7
p399507 <p399507@localdomain>
2018-01-20 13:38:59 +0100

    news funktioniert

--- Configuration/TypoScript/all/plugin.ts
+++ Configuration/TypoScript/all/plugin.ts
@@ -4,23 +4,40 @@
 
 plugin.tx_news {
 
-	rss.channel {
-		title = Thomas Wöhlke
-		description =
-		link = https://woehlke.org/
-		language = de-de
-		copyright = Thomas Wöhlke
-		category = news
-		generator = TYPO3 EXT:news
+	settings {
+
+		list {
+			# media configuration
+			media {
+				image {
+					maxWidth = 100
+					maxHeight = 100
+				}
+				dummyImage = typo3conf/ext/woehlkeorg_sitepackage/Resources/Public/img/port80guru-mask-td-thief-small-square.png
+			}
+		}
 	}
 
+	# Opengraph implementation
 	opengraph {
-		site_name = Thomas Wöhlke
+		site_name = {$plugin.tx_news.opengraph.site_name}
+		type = article
+		admins = Thomas Wöhlke
+		email = thomas.woehlke@gmail.com
+		phone_number =
+		fax_number =
+		latitude = 52.5279109
+		longitude = 13.3306969
+		street-address = Emdener Str. 56
+		locality = Berlin
+		region = DE-BE
+		postal-code = 10551
+		country-name = Deutschland
 
 		twitter {
-			card = summary
-			site = https://woehlke.org/
-			creator = @port80guru
+			card = {$plugin.tx_news.opengraph.twitter.card}
+			site = {$plugin.tx_news.opengraph.twitter.site}
+			creator = {$plugin.tx_news.opengraph.twitter.creator}
 		}
 	}
 }
