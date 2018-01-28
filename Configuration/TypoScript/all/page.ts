5838c1385f1b11c25931715d2a5234f54bbaf553
p399507 <p399507@localdomain>
2018-01-28 23:06:01 +0100

    next try...

--- Configuration/TypoScript/all/page.ts
+++ Configuration/TypoScript/all/page.ts
@@ -1,22 +1,60 @@
 page {
+
 	includeCSS {
+
 		cssFileGoogleFontJosefinSans = https://fonts.googleapis.com/css?family=Josefin+Sans
+
 		cssFileGoogleFontRaleway = https://fonts.googleapis.com/css?family=Raleway
-		bootstrapFour = https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css
-		cssFileMyOwn = EXT:woehlkeorg_sitepackage/Resources/Public/css/my.css
+
+		bootstrapFour = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Bootstrap/4.0.0/scss/bootstrap.scss
+		bootstrapFour.forceOnTop = 1
+		bootstrapFour.if.isFalse = {$bootstrap.extconf.customScss}
+
+		customBootstrap = uploads/tx_t3sbootstrap/bootstrap.scss
+		customBootstrap.forceOnTop = 1
+		customBootstrap.if.isTrue = {$bootstrap.extconf.customScss}
+
+		t3sfontawesome4 = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Fontawesome/4.0.7/css/font-awesome.min.css
+		t3sfontawesome4.if.value = {$bootstrap.config.fontawesomeversion}
+		t3sfontawesome4.if.equals = 4
+
+		t3sfontawesome5 = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Fontawesome/5.0.6/on-server/css/fontawesome-all.css
+		t3sfontawesome5.if.value = {$bootstrap.config.fontawesomeversion}
+		t3sfontawesome5.if.equals = 5
+
+		t3sfontawesome4link = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Fontawesome/fontAwesomeLink4.css
+		t3sfontawesome4link.if.value = {$bootstrap.config.fontawesomeversion}
+		t3sfontawesome4link.if.equals = 4
+		t3sfontawesome5link = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Fontawesome/fontAwesomeLink5.css
+		t3sfontawesome5link.if.value = {$bootstrap.config.fontawesomeversion}
+		t3sfontawesome5link.if.equals = 5
+
+		cssFileMyOwn = EXT:woehlkeorg_sitepackage/Resources/Public/Styles/my.css
 	}
-	includeJSFooterlibs {
+	includeJSLibs {
+		fontawesome = https://use.fontawesome.com/releases/v5.0.6/js/all.js
+		fontawesome.external = 1
+		fontawesomeShims =  https://use.fontawesome.com/releases/v5.0.6/js/v4-shims.js
+		fontawesomeShims.external = 1
 		jsTwitterWidget = https://platform.twitter.com/widgets.js
 		jsTwitterWidget.external = 1
+	}
+	includeJSFooterlibs {
+		jquery = https://code.jquery.com/jquery-3.2.1.slim.min.js
+		jquery.external = 1
 		t3sbjquery = https://code.jquery.com/jquery-3.2.1.slim.min.js
 		t3sbjquery.forceOnTop = 1
 		t3sbpopper = https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js
+		t3sbpopper.external = 1
 		t3sbootstrap = https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
+		t3sbootstrap.external = 1
 	}
 	includeJSFooter {
-		#jquery = EXT:indexed_search_autocomplete/Resources/Public/JavaScript/Jquery-3.2.1.min.js
-		#jsMyJquery = EXT:indexed_search_autocomplete/Resources/Public/JavaScript/Jquery-3.2.1.min.js
-		jsFileMyOwn = EXT:woehlkeorg_sitepackage/Resources/Public/js/my.js
+		t3sbjquery = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Jquery/jquery-3.2.1.slim.min.js
+		jquery = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Jquery/jquery-3.2.1.slim.min.js
+		t3sbpopper = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Popper/1.12.9/umd/popper.min.js
+		t3sbootstrap = EXT:woehlkeorg_sitepackage/Resources/Public/Contrib/Bootstrap/4.0.0/dist/js/bootstrap.min.js
+		jsFileMyOwn = EXT:woehlkeorg_sitepackage/Resources/Public/JavaScripts/my.js
 	}
 	10 {
 		layoutRootPaths {
@@ -68,9 +106,9 @@ page {
 		headerComment (
 --------------------------------------------------
 |                                                |
-|  TYPO3 Website - Development by                |
+|  TYPO3 Integration and Development             |
 |                                                |
-|  Thomas Woehlke | 2017 | woehlke.org           |
+|  2018 | Thomas Woehlke | woehlke.org           |
 |                                                |
 --------------------------------------------------
 		)
